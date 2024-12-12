a1785dc50ef9b22a46ed524a0326f1f8f0f45216
AlexanderMundt <amundt123@gmail.com>
2024-12-12 13:04:46 -0500

    Added myself in the data

--- serverside/src/main/resources/data.sql
+++ serverside/src/main/resources/data.sql
@@ -20,6 +20,8 @@ VALUES ('White bread', 54 ,'One loaf',1.5 ,2.99);
 INSERT INTO Employee (Title,firstlast,Company,Password,Email)
  VALUES ('Manager','Michael Tracy','Fanshawe','','m');
  INSERT INTO Employee (Title,firstlast,Company,Password,Email)
+ VALUES ('Manager','Alexander Mundt','Fanshawe2','','m');
+ INSERT INTO Employee (Title,firstlast,Company,Password,Email)
  VALUES ('Employee','Michael Tracy2','Fanshawe','','mt');
  INSERT INTO Employee (Title,firstlast,Company,Password,Email)
  VALUES ('Employee','Michael Tracy3','Fanshawe','password3','mt3@abc.com');
